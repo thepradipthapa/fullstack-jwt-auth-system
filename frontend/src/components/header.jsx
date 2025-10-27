1b7ab839fee05dddb93a0af87294c9c30eff87d2
Pradip Thapa <thepradipthapa.404@gmail.com>
2025-10-27 15:01:54 +0545

    fix: add React Link to remove page refresh

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Button from './Button'
+import { Link } from 'react-router'
 
 const header = () => {
   return (
        <>
         <nav className='navbar container mt-3'>
-            <a className='navbar-brand text-light' href="">FullStack JWT Auth System</a>
+            <Link className='navbar-brand text-light' to="/">FullStack JWT Auth System</Link>
 
         <div>
-            <Button text='Login' class='btn-danger' />
+            <Button text='Login' class='btn-danger' url='/login'/>
             &nbsp; &nbsp;
-            <Button text='Register' class='btn-outline-danger' />
+            <Button text='Register' class='btn-outline-danger' url='/register'/>
         </div>
         </nav>
     </>
